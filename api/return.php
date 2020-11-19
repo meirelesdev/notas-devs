bf32babd2555bba8274de96521388d035a66c8ec
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 08:44:18 -0300

    --

--- api/return.php
+++ api/return.php
@@ -1,8 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: * ");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+// header('Content-Type: application/json');
 
 echo json_encode($array);
 die();
