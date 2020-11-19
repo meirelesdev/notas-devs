49f69af63db771bf75072a2f48669567bdc7fc5a
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 07:59:57 -0300

    mais um update

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: * ");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json");
 
 header("Location: api/");
 exit;
