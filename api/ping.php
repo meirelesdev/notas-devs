c61b6de2152208bcb84e95c32be07c0adf2ae4d9
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 07:38:54 -0300

    deploy update

--- api/ping.php
+++ api/ping.php
@@ -1,9 +1,8 @@
 <?php
-require('../config.php');
-
 
 $array['result'] = [
             'pong' => true
         ];
+
 
 require('../return.php');
--- return.php
+++ return.php
@@ -1,8 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json");
 
 echo json_encode($array);
 exit;
