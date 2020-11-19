c58a31f2f71cd1865f942ce4cb8dd873e87e0299
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 07:26:33 -0300

    deploy heroku

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-$db_host = 'localhost';
+$db_host = 'us-cdbr-east-05.cleardb.net';
 $db_port = '3306';
-$db_name = 'devsnotes';
-$db_user = 'root';
-$db_pass = 'root';
+$db_name = 'heroku_8df5d5d666ba6b7';
+$db_user = 'b0f29a6f31418d';
+$db_pass = 'e736a404';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host;port=$db_port", $db_user, $db_pass);
 
