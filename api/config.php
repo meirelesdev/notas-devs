df198f4190c0c52ec20cefa963b7021f960cf778
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 09:51:29 -0300

    atualizando header

---
+++ api/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = 'us-cdbr-east-05.cleardb.net';
+$db_port = '3306';
+$db_name = 'heroku_8df5d5d666ba6b7';
+$db_user = 'b0f29a6f31418d';
+$db_pass = 'e736a404';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host;port=$db_port", $db_user, $db_pass);
+
+
+
+
+?>
--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
-require('../config.php');
+require('config.php');
+
 $array = [
     "error" => "",
     "result" => []
@@ -9,23 +10,23 @@ $method = strtolower($_SERVER['REQUEST_METHOD']);
 if (isset($_GET['url']) && !empty($_GET['url'])) {
     $url = $_GET['url'];
     switch ($url) {
-        // ok
+
         case 'cadastrar':            
             require "insert.php";
         break;
-        // ok
+
         case 'listar':
             require "getall.php";
         break;
-        // ok
+
         case 'deletar':
             require "delete.php";
         break;
-        // ok
+
         case 'atualizar':
             require "update.php";
         break;
-        // ok
+
         case 'exibir':
             require "get.php";
             break;
@@ -34,6 +35,5 @@ if (isset($_GET['url']) && !empty($_GET['url'])) {
             break;
     }
 } else {
-    // ok
     require "getall.php";
 }
