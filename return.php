df02f522e96a0ed321b5cc3efd60ff896b540c27
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-19 08:11:22 -0300

    update

--- return.php
+++ return.php
@@ -1,6 +1,8 @@
 <?php
 
-
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: * ");
 
 echo json_encode($array);
 exit;
